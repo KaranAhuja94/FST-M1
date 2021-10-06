df8d1ee478444f88c6151cebe4d0f7ccae60b5cf
KaranAhuja94 <32140617+KaranAhuja94@users.noreply.github.com>
2021-10-06 11:09:43 +0530

    Add files via upload

--- Selenium/Project/Activity9.java
+++ Selenium/Project/Activity9.java
@@ -2,8 +2,11 @@ package projectActivities;
 
 import static org.testng.Assert.assertEquals;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -23,7 +26,7 @@ public class Activity9 {
 		driver.manage().window().maximize();
 	}
 	
-	@Test
+	@Test(priority=0)
 	public void completeLesson() {
 		driver.findElement(By.xpath("//a[text()='My Account']")).click();
 		driver.findElement(By.xpath("//a[text()='Login']")).click();
@@ -36,8 +39,27 @@ public class Activity9 {
 		String pageTitle = driver.getTitle();
 		assertEquals(pageTitle, "Developing Strategy – Alchemy LMS");
 		
-		//Mark complete button not available on page
+		//Mark complete button not available on page since course was already completed
 		
+	}
+	
+	@Test(priority=1)
+	public void checkForMarkCompleteButton() {
+		driver.findElement(By.xpath("//a[text()='My Account']")).click();
+		driver.findElement(By.xpath("//a[text()='Login']")).click();
+		driver.findElement(By.xpath("//input[@id='user_login']")).sendKeys("root");
+		driver.findElement(By.xpath("//input[@id='user_pass']")).sendKeys("pa$$w0rd");
+		driver.findElement(By.xpath("//input[@id='wp-submit']")).click();
+		driver.findElement(By.xpath("//a[text()='All Courses']")).click();
+		driver.findElement(By.xpath("//h3[contains(text(),'Content')]/following-sibling::p/a")).click();
+		driver.findElement(By.xpath("//div[contains(text(),'Analyze')]")).click();
+		String pageTitle = driver.getTitle();
+		assertEquals(pageTitle, "Analyze Content & Develop Writing Strategies – Alchemy LMS");
+		List<WebElement> button = driver.findElements(By.xpath("//input[@class='learndash_mark_complete_button']"));
+		for (WebElement webElement : button) {
+			webElement.click();
+			break;
+		}
 	}
 	
 	@AfterMethod
